8b316a45e8791a5f3639ef9a281195f5f1d4a2d2
yulineko <yulineko@gmail.com>
2020-01-26 18:40:32 -0500

    fetch

--- src/Map.js
+++ src/Map.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import GoogleMapReact from 'google-map-react'
 
+const ISS_URL = "http://api.open-notify.org/iss-now.json"
+
 class Map extends React.Component{
     state = {
         center: {
@@ -9,7 +11,28 @@ class Map extends React.Component{
         },
         zoom: 1
     }
+
+    componentDidMount(){
+        // this.getCoordinates()
+        fetch(ISS_URL)
+            .then(res => res.json())
+            .then(data => this.setState({
+                center: {
+                    lat: data.iss_position.latitude,
+                    lng: data.iss_position.longitude
+                }
+            }))
+    }
+
+    // getCoordinates = () => {
+    //     fetch(ISS_URL)
+    //         .then(res => res.json())
+    //         .then(data => console.log(data))
+    // }
+
     render(){
+        console.log("LAT:", this.state.center.lat)
+        console.log("LNG:", this.state.center.lng)
         return(
             <div>
                 MAP PLACEHOLDER
